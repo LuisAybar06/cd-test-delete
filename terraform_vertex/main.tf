02f36a52e811ea0aa6e0c5e0a578e240898b77e1
PC <aluisaybar@gmail.com>
2025-01-11 10:59:14 -0500

    test9

--- terraform_vertex/main.tf
+++ terraform_vertex/main.tf
@@ -1,62 +1,61 @@
-provider "google"{
-    project=var.project_id
-    region=var.region
-}
-
-resource "google_bigquery_dataset" "dataset"{
-    dataset_id=var.dataset_id
-    project=var.project_id
-    location=var.region
+provider "google" {
+  project = var.project_id
+  region  = var.region
 }
 
 
-resource "google_bigquery_table" "table1"{
-    table_id=var.table_id
-    dataset_id=google_bigquery_dataset.dataset.dataset_id
-    project=var.project_id
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = var.dataset_id
+  project    = var.project_id
+  location   = var.region
+}
+ 
 
-schema = jsonencode([
+resource "google_bigquery_table" "table1" {
+  table_id   = var.table_id
+  dataset_id = google_bigquery_dataset.dataset.dataset_id
+  project    = var.project_id
+
+  schema = jsonencode([
     {
-        name="age"
-        type="INTEGER"
-        mode="REQUIRED"
+      name = "age"
+      type = "INTEGER"
+      mode = "REQUIRED"
     },
     {
-        name="workclass"
-        type="STRING"
-        mode="REQUIRED"
+      name = "workclass"
+      type = "STRING"
+      mode = "NULLABLE"
     },
     {
-        name="occupation"
-        type="STRING"
-        mode="REQUIRED"
+      name = "occupation"
+      type = "STRING"
+      mode = "NULLABLE"
     }
-])
-
+  ])
 }
 
 
-resource "google_bigquery_dataset_iam_binding" "dataset_acces_editor"{
-    dataset_id =  google_bigquery_dataset.dataset.dataset_id
-    project = var.project_id
+resource "google_bigquery_dataset_iam_binding" "dataset_access_editor" {
+  dataset_id = google_bigquery_table.table1.dataset_id
+  project    = google_bigquery_table.table1.project
 
-    role = "roles/bigquery.dataEditor"
-    members = [
-        "serviceAccount: mlops-process@trim-odyssey-390415.iam.gserviceaccount.com"
-    ]
+  role    = "roles/bigquery.dataEditor"
+  members = [
+    "serviceAccount:mlops-process@trim-odyssey-390415.iam.gserviceaccount.com"
+  ]
 }
 
 
-resource "google_bigquery_dataset_iam_binding" "dataset_acces_viewer"{
-    dataset_id =  google_bigquery_dataset.dataset.dataset_id
-    project = var.project_id
+resource "google_bigquery_dataset_iam_binding" "dataset_access_viewer" {
+  dataset_id = google_bigquery_table.table1.dataset_id
+  project    = google_bigquery_table.table1.project
 
-    role = "roles/bigquery.dataViewer"
-    members = [
-        "serviceAccount: mlops-process@trim-odyssey-390415.iam.gserviceaccount.com"
-    ]
+  role    = "roles/bigquery.dataViewer"
+  members = [
+    "serviceAccount:mlops-process@trim-odyssey-390415.iam.gserviceaccount.com"
+  ]
 }
-
 
 
 resource "google_bigquery_routine" "cencus_filter_by_age" {
